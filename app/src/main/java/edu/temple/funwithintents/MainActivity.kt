d260a8afbab825686d7a1ae7952e840f78ab8ec6
EthanUngGit <ethan.ung@temple.edu>
2024-04-03 15:23:07 -0400

    Changed the functionability of the button to share text via... and make a Toast when empty

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,19 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val shareText = editText.text.toString()
 
+            // whenever text is Not Empty then implement button
+            if (shareText.isNotEmpty()) {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+                startActivity(Intent.createChooser(shareIntent, "Share text via..."))
+            }
+            else {
+                // sends a Toast whenever text is empty
+                Toast.makeText(this, "Please enter some text", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
